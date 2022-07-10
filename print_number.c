3cc341ef5c4b5bf459eee20111cee61ee730de4b
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-07-10 14:27:06 +0200

    Testing

--- print_number.c
+++ print_number.c
@@ -1,4 +1,22 @@
 #include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - This function returns the length of a string
+ *
+ * @s: String to be counted
+ *
+ * Return: The number of characters in s
+ **/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
 
 /**
  * print_numbr - function
@@ -8,10 +26,22 @@
  */
 int print_numbr(int n, int num)
 {
-	if (n / 10)
-		print_numbr(n / 10, num);
+	char str[65];
+	int i = 0;
 
-	num += _putchar((n % 10) + '0');
+	while (n / 10)
+	{
+		str[i] = ((n % 10) + '0');
+		n = n / 10;
+		i++;
+	}
+	str[i] = n + '0';
+	i = _strlen(str) - 1;
+	while (i >= 0)
+	{
+		num += _putchar(str[i]);
+		i--;
+	}
 
 	return (num);
 }
